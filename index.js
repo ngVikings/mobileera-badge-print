cc9bd0b8d00c7f01786be4f2f42f120ae8e07580
Johannes Brodwall <johannes@brodwall.com>
2016-10-15 00:51:37 +0200

    Extract creating participant information into separate functions

--- index.js
+++ index.js
@@ -1,33 +1,58 @@
 var fs = require('fs')
 
-
-var XLSX = require('xlsx');
-var workbook = XLSX.readFile('3908008463.xls');
-var worksheet = workbook.Sheets[workbook.SheetNames[0]]; 
-var participants = XLSX.utils.sheet_to_json(worksheet);
-
 var yaml = require('node-yaml');
 var speakers = yaml.readSync('data/speakers-data.yml');
 var schedules = yaml.readSync('data/schedule-data.yml');
 var sessions = yaml.readSync('data/sessions-data.yml');
 
-var PDFDocument = require('pdfkit');
-var qr = require('qr-image')
+var XLSX = require('xlsx');
+var workbook = XLSX.readFile('3908008463.xls');
+var worksheet = workbook.Sheets[workbook.SheetNames[0]]; 
+var participantsRaw = XLSX.utils.sheet_to_json(worksheet);
 
-// Create a document
-doc = new PDFDocument({
-   size: 'A6',
-   autoFirstPage: false
-});
-doc.pipe(fs.createWriteStream('badges.pdf'));
+function speakerInfo(participant, speakers, schedules, sessions) {
+   var speakerObj = null;
+   for (var speaker of speakers) {
+      if (speaker.name === participant['Fornavn'] && speaker.surname === participant['Etternavn']) {
+         speakerObj = speaker;
+      }
+   }
+   if (!speakerObj) {
+      return null;
+   }
 
-// Embed a font, set the font size, and render some text
+   var sessionObj = null;
+   for (var session of sessions) {
+      if (session.speakers && session.speakers.indexOf(speakerObj.id) >= 0) {
+         sessionObj = session;
+      }
+   }
+   if (!sessionObj) {
+      return null;
+   }
 
-for (var participant of participants) {
+   var timeInfo = null;
+   for (var schedule of schedules) {
+      for (var timeslot of schedule.timeslots) {
+         if (!timeslot.sessionIds) continue;
+         var sessionIndex = timeslot.sessionIds.indexOf(sessionObj.id);
+         if (sessionIndex >= 0) {
+            return {
+               title: sessionObj.title,
+               date: schedule.dateReadable,
+               timeslot: timeslot.startTime,
+               track: schedule.tracks[sessionIndex].title
+            };
+         }
+      }               
+   }
+   return null;
+}
+
+function createParticipant(participant, speakers, schedules, sessions) {
    var fullName = participant['Fornavn'] + " " + participant["Etternavn"];
-   var contactCard = fullName + " <" + participant["Epost"] + ">";
+   var contactCard = fullName + " <" + participant["Epost"] + "> of " + participant['Company'];
    var sessionInfo = null;
-   //console.log(participant['Company'] + " " + participant["Epost"]);
 
    var ticketName = participant['Billettkategori'];
    var categoryName = ticketName;
@@ -47,61 +72,60 @@ for (var participant of participants) {
       categoryName = 'Normal';
    } else if (ticketName.indexOf('Mobile Era Speaker') === 0) {
       categoryName = 'Speaker';
-
-      var speakerObj = null;
-      for (var speaker of speakers) {
-         if (speaker.name === participant['Fornavn'] && speaker.surname === participant['Etternavn']) {
-            speakerObj = speaker;
-         }
-      }
-      if (speakerObj) {
-         var sessionObj = null;
-         for (var session of sessions) {
-            if (session.speakers && session.speakers.indexOf(speakerObj.id) >= 0) {
-               sessionObj = session;
-            }
-         }
-         if (sessionObj) {
-            var timeInfo = null;
-            for (var schedule of schedules) {
-               for (var timeslot of schedule.timeslots) {
-                  if (!timeslot.sessionIds) continue;
-                  var sessionIndex = timeslot.sessionIds.indexOf(sessionObj.id);
-                  if (sessionIndex >= 0) {
-                     timeInfo = schedule.dateReadable + " " + timeslot.startTime + " in " + schedule.tracks[sessionIndex].title;
-                  }
-               }               
-            }
-            if (timeInfo) {
-               sessionInfo = sessionObj.title + " " + timeInfo;
-            } else {
-               console.log("Unscheduled speaker " + fullName);
-            }
-         } else {
-            console.log("unknown session for speaker " + fullName);
-         }
-      } else {
+      sessionInfo = speakerInfo(participant, speakers, schedules, sessions);
+      if (!sessionInfo) {
          console.log("Unknown speaker " + fullName);
       }
    } else {
       console.log("Unknown category " + categoryName);
    }
+   return { fullName, contactCard, categoryName, sessionInfo };
+}
 
+function printParticipant(doc, participant) {
    doc
       .addPage()
       .image('images/Mobile era oslo 2016 white-01.jpg')
       .font('fonts/PalatinoBold.ttf')
       .fontSize(15);
-   doc.text(fullName, 100, 200)
-      .text(categoryName);
+   doc.text(participant.fullName, 100, 200)
+      .text(participant.categoryName);
+   var sessionInfo = participant.sessionInfo;
    if (sessionInfo) {
-      doc.fontSize(10).text(sessionInfo);
+      doc.fontSize(10)
+         .text(sessionInfo.title)
+         .text(sessionInfo.date + " " + sessionInfo.timeslot +  " in " + sessionInfo.track);
    }
-   doc.image(qr.imageSync(contactCard, {type: 'png'}), {
+   doc.image(qr.imageSync(participant.contactCard, {type: 'png'}), {
          width: 60
-      })
+      });
 }
 
+
+var participants = participantsRaw.map(function(participant) {
+   return createParticipant(participant, speakers, schedules, sessions);
+}).sort(function(a, b) {
+   return a.fullName.localeCompare(b.fullName);
+});
+
+
+var PDFDocument = require('pdfkit');
+var qr = require('qr-image')
+
+// Create a document
+doc = new PDFDocument({
+   size: 'A6',
+   autoFirstPage: false
+});
+doc.pipe(fs.createWriteStream('badges.pdf'));
+
+
+
+for (var participant of participants) {
+   printParticipant(doc, participant);
+   // Back page
+   printParticipant(doc, participant);
+}
 
 // Finalize PDF file
 doc.end();
