f41e1558a7b16a9acb5d5984fb9d0bcdb0bf693a
Maxim Salnikov <salnikov@gmail.com>
2017-01-27 15:54:48 +0100

    code updated

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var participants = require("./participants");
 var badgePrint = require('./badgePrint');
 
 
-badgePrint.badgePrint(participants('./tito-ngvikings-2017-tickets-for-salnikov-gmail-com-1045496.xlsx'), 'badges.pdf');
+badgePrint.badgePrint(participants('./tito-ngvikings-2017-tickets-for-salnikov-gmail-com-1045752.xlsx'), 'badges.pdf');
 
 badgePrint.blankBadgePrint(10, 'blank-badges.pdf');
